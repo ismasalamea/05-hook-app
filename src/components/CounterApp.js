d948a3f39fc4e9c65474b0391e182795b818659f
ismasalamea <ismasalamea@gmail.com>
2022-05-18 00:44:45 -0500

    video81

--- src/components/CounterApp.js
+++ src/components/CounterApp.js
@@ -12,8 +12,8 @@ export const CounterApp = () => {
 
   return (
     <>
-        <h1>Counter 1 {counter1} </h1>
         <h1>Counter 2 {counter2} </h1>
+        <h1>Counter 1 {counter1} </h1>
         <hr />
 
         <button 
