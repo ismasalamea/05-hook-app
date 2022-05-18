29048485ba7a54233925e8d46064a0408b6bb3af
ismasalamea <ismasalamea@gmail.com>
2022-05-18 00:44:00 -0500

    video80

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { CounterApp } from './components/CounterApp';
+import { HookApp } from './HookApp';
+//                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      import { HookApp } from './HookApp';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <CounterApp />
+    
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ 
